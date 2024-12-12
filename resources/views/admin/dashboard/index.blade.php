545c9523cc20521bfb4c4afddd3166008899c869
Robialdy <fachrirobialdy07@gmail.com>
2024-12-12 19:05:51 +0700

    integrasi template admin

---
+++ resources/views/admin/dashboard/index.blade.php
@@ -0,0 +1,37 @@
+@extends('template.template')
+
+@section('content')
+
+<div class="page-title">
+    <div class="row">
+        <div class="col-12 col-md-6 order-md-1 order-last">
+            <h3>Dashboard</h3>
+            <p class="text-subtitle text-muted">You Can See Request.</p>
+        </div>
+        <div class="col-12 col-md-6 order-md-2 order-first">
+            <nav aria-label="breadcrumb" class="breadcrumb-header float-start float-lg-end">
+                <ol class="breadcrumb">
+                    {{-- <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li> --}}
+                    <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                </ol>
+            </nav>
+        </div>
+    </div>
+</div>
+    <section class="section">
+
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Ini Dashboard</h4>
+                    </div>
+                    <div class="card-body">
+                        <h3>...</h3>
+                    </div>
+                </div>
+            </div>
+
+    </section>
+
+@endsection
